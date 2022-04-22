0b7e74d4d43119933d3ee5821a2ef840315a3313
RitikParashar <101326771+RitikParashar@users.noreply.github.com>
2022-04-22 13:42:43 +0530

    Update and rename README.md to EnergyMeter.h

---
+++ 3_Implementation/inc/EnergyMeter.h
@@ -0,0 +1,21 @@
+/**
+ * @file EngergyMeter.h
+ * @author Ritik Parashar 
+ * @brief 
+ * @version 0.1
+ * @date 2022/04/18
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <avr/io.h>
+#define F_CPU 1000000
+#include <util/delay.h>
+#include <stdlib.h>
+
+#define enable            5
+#define registerselection 6
+
+void send_a_command(unsigned char command);
+void send_a_character(unsigned char character);
+void send_a_string(char *string_of_characters);
